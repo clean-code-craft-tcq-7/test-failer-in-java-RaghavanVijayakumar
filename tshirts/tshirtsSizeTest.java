f8b6595a9402f47bbbc033de94334fa8195a41b6
RaghavanVijayakumar <115541217+RaghavanVijayakumar@users.noreply.github.com>
2022-11-28 15:46:57 +0530

    Create tshirtsSizeTest.java

---
+++ tshirts/tshirtsSizeTest.java
@@ -0,0 +1,11 @@
+public class TshirtsSizeTest {
+	public static void main(String[] args) {
+		assert (Tshirts.size(37) == "S");
+		assert (Tshirts.size(38) == "M");
+		assert (Tshirts.size(40) == "M");
+		assert (Tshirts.size(43) == "L");
+		assert (Tshirts.size(0) != "S");
+		assert (Tshirts.size(-50) != "S");
+		System.out.println("All is well (maybe!)");
+	}
+}
