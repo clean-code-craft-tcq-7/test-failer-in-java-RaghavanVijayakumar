02d4facb55909240ba728769317f55f284413f03
RaghavanVijayakumar <115541217+RaghavanVijayakumar@users.noreply.github.com>
2022-11-29 09:13:03 +0530

    Update alerter.java

--- alerter/alerter.java
+++ alerter/alerter.java
@@ -1,4 +1,4 @@
-public class alerter {
+public class Alerter {
 	static int alertFailureCount = 0;
 
 	static int networkAlerter(float celcius, boolean isProductionCode) {
@@ -24,9 +24,23 @@ public class alerter {
 			alertFailureCount += 0;
 		}
 	}
-
-	public static void main(String args[]) {
-		AlerterTest.assertAlerterResponse();
-	}
-
+	static int networkAlertStub(float celcius) {
+    	if(celcius>0) {
+    		System.out.println("ALERT: Temperature is networkstub" + celcius + " celcius");
+    		 return 200; 
+    	}
+    	return 500;
+    }
+    
+	 public static void main(String args[]) {
+		    Alerter.alertInCelcius(400.5f,false);
+		    Alerter.alertInCelcius(303.6f,false);
+		    assert(Alerter.alertFailureCount==0);
+		    Alerter.alertInCelcius(-200.0f,false);
+		    assert(Alerter.alertFailureCount==1);
+		    Alerter.alertInCelcius(0, false);
+		    assert(Alerter.alertFailureCount==2);
+		    System.out.printf("%d alerts failed.\n", Alerter.alertFailureCount);
+		    System.out.println("All is well (maybe!)\n");
+		    }
 }
