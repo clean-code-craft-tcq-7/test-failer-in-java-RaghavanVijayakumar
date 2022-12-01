d478dc1e428aae3ab1f4daf8a18b6461df7c2bd7
RaghavanVijayakumar <115541217+RaghavanVijayakumar@users.noreply.github.com>
2022-12-01 09:49:01 +0530

    Update tshirts.java

--- tshirts/tshirts.java
+++ tshirts/tshirts.java
@@ -14,8 +14,8 @@ public class Tshirts {
 		assert (Tshirts.size(37) == "S");
 		assert (Tshirts.size(38) == "M");
 		assert (Tshirts.size(40) == "M");
+		 assert(tshirts.size(42) == "L");
 		assert (Tshirts.size(43) == "L");
-		assert (Tshirts.size(0) != "S");
 		assert (Tshirts.size(-50) != "S");
 		System.out.println("All is well (maybe!)");
 	}
