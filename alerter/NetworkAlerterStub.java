1c3689faa3893c0b09e5251186728a6854098991
RaghavanVijayakumar <115541217+RaghavanVijayakumar@users.noreply.github.com>
2022-12-01 10:47:57 +0530

    Update NetworkAlerterStub.java

--- alerter/NetworkAlerterStub.java
+++ alerter/NetworkAlerterStub.java
@@ -1,8 +1,9 @@
 public class NetworkAlerterStub implements AlerterInterface {
-static int networkAlertStub(float celcius) {
-    	if(celcius>200) {
+public	Integer sendTestRequest(float celcius) {
+    	if(celcius>150) {
     		System.out.println("ALERT: Temperature is networkstub" + celcius + " celcius");
     		 return 500; 
     	}
     	return 200;
     }
+}
