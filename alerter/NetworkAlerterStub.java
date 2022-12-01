e5b4e527e484f5252b6af043e3f9e41a5f80ca56
RaghavanVijayakumar <115541217+RaghavanVijayakumar@users.noreply.github.com>
2022-12-01 12:24:53 +0530

    Delete NetworkAlerterStub.java

--- alerter/NetworkAlerterStub.java
+++
@@ -1,9 +0,0 @@
-public class NetworkAlerterStub implements AlerterInterface {
-public	Integer sendTestRequest(float celcius) {
-    	if(celcius>150) {
-    		System.out.println("ALERT: Temperature is networkstub" + celcius + " celcius");
-    		 return 500; 
-    	}
-    	return 200;
-    }
-}
