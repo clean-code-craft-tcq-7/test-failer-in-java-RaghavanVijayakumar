61e9eace1b01fe38ab6e05bb60542debe8249909
RaghavanVijayakumar <115541217+RaghavanVijayakumar@users.noreply.github.com>
2022-12-09 15:02:34 +0530

    Update tshirts.java

--- tshirts/tshirts.java
+++ tshirts/tshirts.java
@@ -1,10 +1,10 @@
 public class Tshirts {
 	static String size(int cms) {
-		if (cms < 38) {
+		if (cms <= 38) {
 			return "S";
 		} else if (cms > 38 && cms < 42) {
 			return "M";
-		} else if (cms > 42) {
+		} else if (cms => 42) {
 			return "L";
 		} else
 			return "INVALID";
@@ -12,10 +12,11 @@ public class Tshirts {
 
 	public static void main(String args[]) {
 		assert (Tshirts.size(37) == "S");
-		assert (Tshirts.size(38) == "INVALID");
+		assert (Tshirts.size(38) == "S");
 		assert (Tshirts.size(40) == "M");
-		assert (Tshirts.size(42) == "INVALID");
+		assert (Tshirts.size(42) == "L");
 		assert (Tshirts.size(43) == "L");
+		assert (Tshirts.size("-50") == "INVALID");
 		System.out.println("All is well (maybe!)");
 	}
 }
